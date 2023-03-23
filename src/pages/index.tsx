25d59a1aec9a76fbc871440c3fa5aec334cc8d9f
ShivWad <shivamwadhawan2000@gmail.com>
2023-03-23 21:43:38 +0530

    Landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,27 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const router = useRouter();
-  useEffect(() => {
-    router.push("/profile");
-  }, [])
+  // useEffect(() => {
+  //   router.push("/profile");
+  // }, [])
   return (
-    <>
-    </>
+
+    <div className='landing-page'>
+      <title>
+        FreeThought
+      </title>
+      <h1 className='landing-page-heading'>
+        Free Thought!
+      </h1>
+      <div className='landing-page-content'>
+        FreeThought is a platform for individuals to share their ideas, opinions, and musings with the world. Our web app provides a safe and welcoming space for users to express themselves freely and connect with like-minded individuals. Whether you're looking to spark a conversation, gain perspective, or simply share your thoughts with others, FreeThought is the perfect platform to do so. Join our community today and let your ideas take flight!
+      </div>
+      <div className='landing-page-actions'>
+        <button className='global-button' onClick={()=>{router.push("/signup")}}>
+          Let's go
+        </button>
+      </div>
+    </div>
+
   )
 }
